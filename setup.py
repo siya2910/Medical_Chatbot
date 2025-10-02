51500a7b4716694ab85f0b9aee9389f23be07de0
Siya Mandal <siyamandal2910@gmail.com>
2025-10-02 16:28:29 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Medical Bot Project",
+    version="0.0.0",
+    author="Siya Mandal",
+    author_email="siyamandal2910@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
